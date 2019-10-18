550b60ea6c3e16a0d2b9d753ba73896adb8fd3cb
Michal Misiarek <michal.misiarek@schibsted.com>
2019-10-18 16:41:43 +0200

    add styling

--- pages/Home.js
+++ pages/Home.js
@@ -1,5 +1,20 @@
 import React from "react";
+import styled from "styled-components";
+import Typography from "@material-ui/core/Typography";
 
-const Home = () => <div>Home</div>;
+const Wrapper = styled.section`
+    padding: 4em;
+    background: papayawhip;
+`;
+
+function Home() {
+    return (
+        <Wrapper>
+            <Typography variant="h2" gutterBottom>
+                Home screen
+            </Typography>
+        </Wrapper>
+    );
+}
 
 export default Home;
