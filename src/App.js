33747b2a801e0ba07606e674e5a8fb6830982139
Michal Misiarek <michal.misiarek@schibsted.com>
2019-10-18 11:22:24 +0200

    add eslint and prettier

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import React, { Component } from "react";
+import React from "react";
 import { hot } from "react-hot-loader";
 
-class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <h1> Hello, World!</h1>
-            </div>
-        );
-    }
-}
+const App = () => (
+    <div className="App">
+        <h1> Hello, World!</h1>
+    </div>
+);
 
 export default hot(module)(App);
